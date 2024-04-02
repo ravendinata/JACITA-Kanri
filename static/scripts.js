5d63978e6c982e524560106118af9ecd0e36fc0c
Raven Limadinata <raven.limadinata@outlook.com>
2024-04-02 11:47:51 +0700

    Made UI color theme persistent
    Originally, the color theme can be changed but upon reload or move to another page, the theme will go back to default. Now, the color theme is persistent across different pages and after reloads.

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,12 @@
 // On ready
 $(document).ready(function ()
 {
+    // Set the theme
+    if (getCookie('theme') == 'dark')
+        document.documentElement.setAttribute('data-bs-theme','dark')
+    else
+        document.documentElement.setAttribute('data-bs-theme','light')
+    
     // Normalize the URL
     window.history.replaceState({}, document.title, window.location.pathname);
 
@@ -16,9 +22,51 @@ document.getElementById('btnToggleColorMode').addEventListener('click',()=>{
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark')
     {
         document.documentElement.setAttribute('data-bs-theme','light')
+        setCookie('theme', 'light', 365)
     }
     else
     {
         document.documentElement.setAttribute('data-bs-theme','dark')
+        setCookie('theme', 'dark', 365)
     }
 });
+
+// Functions
+function setCookie(name, value, days) 
+{
+    var expires = "";
+    
+    if (days) 
+    {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) 
+{
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+
+    for(var i=0;i < ca.length;i++) 
+    {
+        var c = ca[i];
+
+        while (c.charAt(0)==' ') 
+        {
+            c = c.substring(1,c.length);
+        }
+        
+        if (c.indexOf(nameEQ) == 0) 
+        {
+            return c.substring(nameEQ.length,c.length);
+        }
+    }
+
+    return null;
+}
+
+function wipeCookie(name) { document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'; }
