09960ccc75b0d210eb1c55b8c0c85d6ab8892b54
Raven Limadinata <raven.limadinata@outlook.com>
2026-08-13 06:52:38 +0700

    Update app initializer
    - Migrate DB connection information to config instead of hard-coded
    - Add initialization process verbosity
    - Improve error handling with try-catch block

--- app/__init__.py
+++ app/__init__.py
@@ -8,31 +8,51 @@ from app.extensions import db
 with open('config.json', 'r') as f:
     config = json.load(f)
 
+    db_host = config['db_host']
+    db_port = config['db_port']
+    db_name = config['db_name']
     db_username = config['db_username']
     db_password = config['db_password']
 
 def create_app():
+    print("Starting Kanri Backend...")
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_username}:{db_password}@jacita-itam-jacita.a.aivencloud.com:23758/device-inventory?charset=utf8mb4'
+
+    print("Configuring Database...")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}?charset=utf8mb4'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Extensions
+    print("Initializing Database...")
     db.init_app(app)
 
     # ==========
     # Blueprints
     # ==========
-    from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+    print("Registering Blueprints...")
 
-    from app.api import bp as api_bp
-    app.register_blueprint(api_bp, url_prefix = '/api')
+    try:
+        print("Importing Page Blueprint...")
+        from app.main import bp as main_bp
+        app.register_blueprint(main_bp)
+
+        print("Importing API Blueprint...")
+        from app.api import bp as api_bp
+        app.register_blueprint(api_bp, url_prefix = '/api')
+    except ImportError as e:
+        print(f"Error importing blueprints: {e}")
+    except Exception as e:
+        print(f"Unexpected error during blueprint registration: {e}")
 
     # ==============================
     # Error handlers
     # ==============================
+    print("Setting up Error Handlers...")
+
     @app.errorhandler(404)
     def page_not_found(e):
         return render_template('handlers/404.html', title = '404'), 404
+    
+    print("Backend setup complete.")
 
     return app
